4d3ffcf93dabd0726628ebe99d4a062f84958c67
Raja-10706 <rajabattula10@gmail.com>
2025-07-28 11:57:35 +0530

    Setup for GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/hello-world-app/',  // Replace with your actual repo name
+  base: '/zoro/',  // Replace with your actual repo name
   plugins: [react()],
 })
